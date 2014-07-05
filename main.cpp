fa4fb3110aea7cec094d2efd404067b2ae9688fd
trineroks <trineroks@Jasons-MacBook-Air.local>
2014-07-05 13:15:59 -0700

    new files

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include "SDL.h"
+
+int main(int argc, char *argv[])
+{
+    /* initialize SDL */
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("Could not initialize SDL\n");
+        return 1;
+    }
+    
+    SDL_DisplayMode displayMode;
+    SDL_GetDesktopDisplayMode(0, &displayMode);
+    
+    /* create window and renderer */
+    SDL_Window* window = SDL_CreateWindow(NULL, 0, 0, displayMode.w, displayMode.h, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        printf("Could not initialize Window\n");
+        return 1;
+    }
+    
+    auto gl = SDL_GL_CreateContext(window);
+    
+    //Game Loop
+    SDL_Event event;
+    auto done = false;
+    while (!done)
+    {
+        SDL_PumpEvents();
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                    
+                case SDL_APP_DIDENTERFOREGROUND:
+                    SDL_Log("SDL_APP_DIDENTERFOREGROUND");
+                    break;
+                    
+                case SDL_APP_DIDENTERBACKGROUND:
+                    SDL_Log("SDL_APP_DIDENTERBACKGROUND");
+                    break;
+                    
+                case SDL_APP_LOWMEMORY:
+                    SDL_Log("SDL_APP_LOWMEMORY");
+                    break;
+                    
+                case SDL_APP_TERMINATING:
+                    SDL_Log("SDL_APP_TERMINATING");
+                    break;
+                    
+                case SDL_APP_WILLENTERBACKGROUND:
+                    SDL_Log("SDL_APP_WILLENTERBACKGROUND");
+                    break;
+                    
+                case SDL_APP_WILLENTERFOREGROUND:
+                    SDL_Log("SDL_APP_WILLENTERFOREGROUND");
+                    break;
+                    
+                case SDL_WINDOWEVENT:
+                {
+                    switch (event.window.event)
+                    {
+                        case SDL_WINDOWEVENT_RESIZED:
+                        {
+                            SDL_Log("Window %d resized to %dx%d", event.window.windowID, event.window.data1, event.window.data2);
+                            
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    SDL_GL_DeleteContext(gl);
+    
+    // Done! Close the window, clean-up and exit the program.
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
